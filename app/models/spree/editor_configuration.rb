b4dbdc25fce10e49cd77401752d8e0da9aedde02
Babur Usenakunov <bob.usenakunov@gmail.com>
2013-02-11 17:58:07 +0600

    CKEditor as default editor

--- app/models/spree/editor_configuration.rb
+++ app/models/spree/editor_configuration.rb
@@ -1,7 +1,7 @@
 class Spree::EditorConfiguration < Spree::Preferences::Configuration
 
   preference :enabled, :boolean, :default => true
-  preference :current_editor, :string, :default => 'TinyMCE'
+  preference :current_editor, :string, :default => 'CKEditor'
   preference :ids, :text, :default => 'product_description page_body'
 
 end
